c04070db1e128ad9c0957406d9e0121ce76bd494
twknab <20636750+twknab@users.noreply.github.com>
2019-11-29 21:30:42 -0800

    styled logo

---
+++ frontend/pages/index.js
@@ -0,0 +1,7 @@
+const Home = props => (
+  <div>
+    <p>THIS IS HOME!</p>
+  </div>
+);
+
+export default Home;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,7 @@
+const Sell = props => (
+  <div>
+    <p>SELLING STUFF!</p>
+  </div>
+);
+
+export default Sell;
